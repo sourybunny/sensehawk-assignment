47948ac83dd1b554db1e7581f270797e4b62d360
sourybunny <soury.bunny@gmail.com>
2020-03-18 20:50:56 +0530

    feat: bg fade

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,30 +1,56 @@
 <template>
+<div>
+<TaskItem :task="task" class="dummy"/>
   <div class="task-box">
-      <v-text-field id="newtask" @keyup.enter.native="addTask($store.getters.title)" 
-       color="grey lighten-3" full-width v-model="$store.getters.title" placeholder="add new task"></v-text-field>
-      <!-- <v-btn @click="addTask($store.getters.title)">submit</v-btn> -->
+      
+    <v-text-field id="newtask" @keyup.enter.native="addTask($store.getters.title)" 
+       color="grey lighten-3" full-width v-model="$store.getters.title" placeholder=""></v-text-field>   
+  </div>
   </div>
 </template>
-
 <script>
 import {mapGetters,mapMutations} from 'vuex';
+import TaskItem from './TaskItem'
 export default {
-    
     computed: {
         ...mapGetters(['listItems'])
     },
     methods: {
         ...mapMutations(['addTask'])
+    },
+    data(){
+        return {
+            task: {
+                id:'#',
+                title: '',
+                completed: false,
+                type: 'spotify'
+            }
+        }
+    },
+    components: {
+        TaskItem
     }
 }
 </script>
 
 <style>
+
+.task-box{
+    
+    margin-top: -5rem;
+}
+.dummy {
+    background: linear-gradient(0deg, rgba(252, 249, 249, 0.171) 28.21%, #dddddd60 134.95%);
+    opacity: 0.2;
+}
+
 #newtask {
-    opacity: 0.4;
+    /* opacity: 0.7; */
     
 }
-.task-box{
-    background: linear-gradient(0deg, rgba(252, 249, 249, 0.171) 28.21%, #dddddd60 134.95%);
+input {
+    text-indent: 3rem;
 }
+
 </style>
